59457b0ccfd680191d84ada1432eabe585c6d4ce
Kadin Sayani <kadinsayani8@gmail.com>
2022-10-29 00:54:09 -0600

    added start button

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,29 @@
-class Board extends React.Component {
-  // Use the render function to return JSX component
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Board {this.props.name}</h1>
-      </div>
-    );
+// draw grid on canvas
+let width = 100;
+let height = 100;
+let padding = 10;
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+function drawBoard() {
+  for (let x = 0; x <= width; x += 10) {
+    context.moveTo(0.5 + x + padding, padding);
+    context.lineTo(0.5 + x + padding, height + padding);
   }
+
+  for (let x = 0; x <= height; x += 10) {
+    context.moveTo(padding, 0.5 + x + padding);
+    context.lineTo(width + padding, 0.5 + x + padding);
+  }
+
+  context.strokeStyle = "black";
+  context.stroke();
 }
 
-// Obtain the root
-const rootElement = document.getElementById("root");
-// Create a function to wrap up your component
-function App() {
-  return (
-    <div>
-      <Board name="dotsboxes" />
-    </div>
-  );
+function startGame() {
+  drawBoard();
+  console.log("Game started");
 }
 
-// Use the ReactDOM.render to show your component on the browser
-ReactDOM.render(<App />, rootElement);
+startGame();
