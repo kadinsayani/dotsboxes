8cf5954878c150d391159d516fd9001289fb98d3
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-01 14:39:23 -0600

    added board

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,9 @@
 $(document).ready(function () {
   $("p").hide();
-  $("canvas").hide();
+  $(".board").hide();
   $("a").click(function () {
     $("a").hide();
     $("p").show();
-    $("canvas").show();
-    drawBoard();
+    $(".board").show();
   });
 });
-
-// draw grid on canvas
-let width = 100;
-let height = 100;
-let padding = 10;
-
-let canvas = $("canvas");
-let context = canvas.getContext("2d");
-
-function drawBoard() {
-  for (let x = 0; x <= width; x += 10) {
-    context.moveTo(0.5 + x + padding, padding);
-    context.lineTo(0.5 + x + padding, height + padding);
-  }
-
-  for (let x = 0; x <= height; x += 10) {
-    context.moveTo(padding, 0.5 + x + padding);
-    context.lineTo(width + padding, 0.5 + x + padding);
-  }
-
-  context.strokeStyle = "black";
-  context.stroke();
-}
