15795d12b7eaf71c67922995fde8784a4c12ee9a
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-02 13:33:51 -0600

    added start game

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,24 @@ $(document).ready(function () {
     $("p").show();
     $(".board").show();
   });
+  startGame();
 });
+
+function startGame() {
+  const p1 = { score: 0, playerNumber: 1 };
+  const p2 = { score: 0, playerNumber: 2 };
+  const p3 = { score: 0, playerNumber: 3 };
+  console.log("game started");
+
+  let currentPlayer = p1;
+  if (currentPlayer.playerNumber == 1) {
+    //TODO
+    currentPlayer = p2;
+  } else if (currentPlayer.playerNumber == 2) {
+    //TODO
+    currentPlayer = p3;
+  } else {
+    //TODO
+    currentPlayer = p1;
+  }
+}
