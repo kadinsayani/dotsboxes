cabda2f8095d3df8713c12e433ab60711c1be347
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-17 13:13:31 -0700

    updated io listen

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app).listen(8080);
-const io = require("socket.io").listen(server);
-const game = require("../client/scripts/Game.js");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+server.listen(8080);
+//const game = require("../client/scripts/Game.js");
 
 io.sockets.on("connection", (socket) => {
   console.log("client connected");
