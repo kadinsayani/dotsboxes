cc63ba835a8ac61c7559f1d6a8cb0b2931ea7070
Kadin Sayani <kadinsayani8@gmail.com>
2022-11-23 15:08:31 -0700

    added code to server index.html

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-server.listen(8080);
+server.listen(3001);
 //const game = require("../client/scripts/Game.js");
 
 io.sockets.on("connection", (socket) => {
   console.log("client connected");
+});
+
+// serve index.html from ../client
+app.use(express.static("../client"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
